0e9d49465da8281520e5d65e19dc3e4412fdb38d
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-10 10:13:14 +0200

    initial message

---
+++ src/test/java/com/example/accessrelationaldatausingjdbcwithspring/AccessRelationalDataUsingJdbcWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessrelationaldatausingjdbcwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessRelationalDataUsingJdbcWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
